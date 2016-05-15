6c7666d057a8cdfd5517d49f2098903972ce5c2c
laozhuangg <laozhuangg@gmail.com>
2016-05-15 11:56:32 +0800

    remove api key

--- app/src/main/java/com/example/popularmovies/Constants.java
+++ app/src/main/java/com/example/popularmovies/Constants.java
@@ -7,5 +7,5 @@ public class Constants {
     public static final String BASE_URL = "https://api.themoviedb.org/3/";
     public static final String BASE_POSTER_URL = "http://image.tmdb.org/t/p/w185";
 
-    public static final String API_KEY = "a1bfbed3eae995b933f86c9f60207b69";
+    public static final String API_KEY = "";
 }
